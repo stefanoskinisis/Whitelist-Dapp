4ea99a52cba2ef283a09aa96655c7ab03891effc
stefanoskinisis <stefanoskinisis@gmail.com>
2022-09-27 12:24:09 +0300

    second test

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
+
+// 0x52bee1B40f2451927B8068149aCd4C7B011fa28B
